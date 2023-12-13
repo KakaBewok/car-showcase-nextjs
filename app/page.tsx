b308f972b00829e39b6547bbbb0e4e1e13ca4e8d
noprizal <noprizal@voltras.co.id>
2023-12-13 08:28:18 +0700

    fixing filter container

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home({ searchParams }: HomeProps) {
         <div className="home__filters">
           <SearchBar />
 
-          <div className="home__filters-container">
+          <div className="home__filter-container">
             <CustomFilter title="fuel" options={fuels} />
             <CustomFilter title="year" options={yearsOfProduction} />
           </div>
